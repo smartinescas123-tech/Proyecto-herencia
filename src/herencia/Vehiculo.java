946bd3283f20dc788d4bce6a886e7d5e2cb449f0
Tu Nombre <tu_correo@uniempresarial.edu.co>
2026-08-29 04:30:00 -0500

    se le añade metodos a la clase vehiculo

--- src/herencia/Vehiculo.java
+++ src/herencia/Vehiculo.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package herencia;
 
-/**
- *
- * @author Usuario
- */
 public class Vehiculo {
-    
+    protected String marca;
+    protected String modelo;
+    protected double velocidadMaxima;
+
+    public Vehiculo(String marca, String modelo, double velocidadMaxima) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public void acelerar() {
+        System.out.println(marca + " " + modelo + " está acelerando.");
+    }
+
+    public void frenar() {
+        System.out.println(marca + " " + modelo + " está frenando.");
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Marca: " + marca + " | Modelo: " + modelo + " | Vel. máx: " + velocidadMaxima + " km/h");
+    }
 }
